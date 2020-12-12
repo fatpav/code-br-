a20b6704f2757efdf8f4d5c7e9fd393655c092dc
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-12 15:26:23 +0000

    JavaSpring app initialised and added to git to allow start of back-end class and DB fleshing out

---
+++ code_br_server_side/src/main/java/com/codeclan/example/CodeBrServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBrServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBrServerSideApplication.class, args);
+	}
+
+}
