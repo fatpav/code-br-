5cb861c184db8e5bf12f32dbca0e8d6bc56ea086
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-12 15:45:15 +0000

    re-started Java Sring as initiliser hadnt worked correctly -= now all files present and correct

---
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/CodeBrServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.code_br_server_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBrServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBrServerSideApplication.class, args);
+	}
+
+}
