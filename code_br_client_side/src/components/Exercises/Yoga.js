0b368f2df16107c2412988773455c42ac6221656
fatpav <fat_pav@hotmail.com>
2020-12-14 11:48:22 +0000

    youtube embedded

--- code_br_client_side/src/components/Exercises/Yoga.js
+++ code_br_client_side/src/components/Exercises/Yoga.js
@@ -1,7 +1,13 @@
+import React from 'react';
+
 const Yoga = () => {
     return (
+        <>
         <h2>What's up with you boo boo</h2>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/nQFf38xeBww" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </>
     )
-}
+};
 
 export default Yoga;
