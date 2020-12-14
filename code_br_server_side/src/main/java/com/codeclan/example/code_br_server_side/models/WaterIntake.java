5c688dd3775dbc62c0c7a2a857f254f2d35e3e80
Alan Parley <alanparley@hotmail.com>
2020-12-14 09:54:36 +0000

    WaterIntake model done

--- code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/models/WaterIntake.java
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/models/WaterIntake.java
@@ -1,4 +1,47 @@
 package com.codeclan.example.code_br_server_side.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="water")
+
 public class WaterIntake {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    
+    @Column(name = "litres")
+    private double litres;
+
+    @JsonIgnoreProperties ({"water"})
+    @OneToOne (mappedBy = "water", fetch = FetchType.LAZY)
+    private DiaryEntry diaryEntry;
+
+    public WaterIntake(double litres) {
+        this.litres = litres;
+    }
+
+    public WaterIntake (){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLitres() {
+        return litres;
+    }
+
+    public void setLitres(double litres) {
+        this.litres = litres;
+    }
 }
