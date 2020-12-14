f54c06cdf962946fd8a015ecdcc95c4d12579b8b
Alan Parley <alanparley@hotmail.com>
2020-12-14 10:13:12 +0000

    meditation model done

--- code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/models/Meditation.java
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/models/Meditation.java
@@ -1,4 +1,58 @@
 package com.codeclan.example.code_br_server_side.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="meditation")
+
 public class Meditation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name = "minutes")
+    private int minutes;
+
+    @Column (name = "completed")
+    private boolean completed;
+
+    @JsonIgnoreProperties({"meditation"})
+    @OneToOne (mappedBy = "meditation", fetch = FetchType.LAZY)
+    private DiaryEntry diaryEntry;
+
+    public Meditation(int minutes, boolean completed) {
+        this.minutes = minutes;
+        this.completed = completed;
+    }
+
+    public Meditation (){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
