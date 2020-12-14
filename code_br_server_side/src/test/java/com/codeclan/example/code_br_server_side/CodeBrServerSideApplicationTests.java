068e5487da634bed69d18fd07987f8c16a51a541
Alan Parley <alanparley@hotmail.com>
2020-12-14 12:34:46 +0000

    user class tests running

--- code_br_server_side/src/test/java/com/codeclan/example/code_br_server_side/CodeBrServerSideApplicationTests.java
+++ code_br_server_side/src/test/java/com/codeclan/example/code_br_server_side/CodeBrServerSideApplicationTests.java
@@ -1,8 +1,9 @@
 package com.codeclan.example.code_br_server_side;
-
 import com.codeclan.example.code_br_server_side.models.User;
+import org.junit.Before;
+//import com.codeclan.example.code_br_server_side.models.User;
 import com.codeclan.example.code_br_server_side.repositories.UserRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,21 +12,36 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class CodeBrServerSideApplicationTests {
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 
-	@Autowired
-	UserRepository userRepository;
+public class CodeBrServerSideApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private User user1;
+	@Before
+	public void setUp(){
+		user1 = new User ("Json", "Jsonson", 33);
 	}
+//	@Autowired
+//	UserRepository userRepository;
+
+//	@Test
+//	public void contextLoads() {
+//	}
 
 	@Test
 	public void canGetUserFirstName(){
-		User user1 = new User ("Json", "Jsonson", 33);
 		assertEquals("Json", user1.getFirst_name());
+	}
+
+	@Test
+	public void canGetUserLastName(){
+		assertEquals("Jsonson", user1.getLast_name());
+	}
+
+	@Test
+	public void canGetUserAge(){
+		assertEquals(33, user1.getAge());
 	}
 
 }
