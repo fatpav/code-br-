cb813e5174e0fa1f7b9425933b737e36b52752dc
Alan Parley <alanparley@hotmail.com>
2020-12-14 11:41:49 +0000

    push all updates to steven

--- code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/components/DataLoader.java
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/components/DataLoader.java
@@ -1,10 +1,31 @@
 package com.codeclan.example.code_br_server_side.components;
 
+import com.codeclan.example.code_br_server_side.models.User;
+import com.codeclan.example.code_br_server_side.repositories.DiaryEntryRepository;
+import com.codeclan.example.code_br_server_side.repositories.JournalEntryRepository;
+import com.codeclan.example.code_br_server_side.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    DiaryEntryRepository diaryEntryRepository;
+
+    public DataLoader(){}
+
+    public void run(ApplicationArguments args){
+        User user1 = new User("Json", "Jsonson", 33);
+        userRepository.save(user1);
+    }
 
 }
--- code_br_server_side/src/test/java/com/codeclan/example/code_br_server_side/CodeBrServerSideApplicationTests.java
+++ code_br_server_side/src/test/java/com/codeclan/example/code_br_server_side/CodeBrServerSideApplicationTests.java
@@ -1,13 +1,31 @@
 package com.codeclan.example.code_br_server_side;
 
+import com.codeclan.example.code_br_server_side.models.User;
+import com.codeclan.example.code_br_server_side.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CodeBrServerSideApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canGetUserFirstName(){
+		User user1 = new User ("Json", "Jsonson", 33);
+		assertEquals("Json", user1.getFirst_name());
 	}
 
 }
