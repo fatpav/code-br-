23fcd010c81c6d3eef6b024e2f9a2c1d616b88cc
fatpav <fat_pav@hotmail.com>
2020-12-14 11:40:51 +0000

    music embedded

--- code_br_client_side/src/components/Exercises/Meditate.js
+++ code_br_client_side/src/components/Exercises/Meditate.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
 const Meditate = () => {
     return (
-        <h2>Come on, we're meditating over here</h2>
+        <>
+            <h2>Come on, we're meditating over here</h2>
 
-        
+            <ReactPlayer url="https://soundcloud.com/user-16685247/sets/codebreak-reiki-zen"></ReactPlayer>
+            
+        </>
     )
-}
+};
 
 export default Meditate;
