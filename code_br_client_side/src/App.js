415c4992278d17cc8892d05346eecde58d52f615
fatpav <fat_pav@hotmail.com>
2020-12-15 21:45:58 +0000

    back and front end talking.  YAY

--- code_br_client_side/src/App.js
+++ code_br_client_side/src/App.js
@@ -22,18 +22,23 @@ import Loading from './components/Loading';
 function App() {
   
   const [onWelcome, setOnWelcome] = useState(true);
+  const [diaryEntries, setDiaryEntries] = useState([{}])
+  const [loaded, setLoaded] = useState(false)
+  const getData = () => {
+    fetch("http://localhost:8080/diaryentry")
+        .then(response => response.json())
+        .then(results => setDiaryEntries(results))
+        .then(() => setLoaded(true))
+        console.log(diaryEntries);
+};
+
 
   useEffect(()=>{
     const loadTimer = setTimeout(() => {
       setOnWelcome(false)
     }, 5000)
-  })
-
-  if(onWelcome === true) {
-    return (
-      <Loading />
-    )
-  }
+    getData();
+  }, [])
 
 
 
@@ -52,7 +57,7 @@ function App() {
             <Route path="/breathe" component={Breathe}/>
             <Route path="/mood" component={Mood}/>
             <Route path="/sleep" component={Sleep}/>
-            <Route path="/detail" component={Detail}/>
+            <Route path="/detail" diaryEntry={diaryEntries} component={Detail}/>
             <Route path="/tracker" component={Tracker}/>
             <Route path="/waterintake" component={WaterIntake}/>
             <Route path="/screentime" component={Screentime}/>
--- code_br_client_side/src/components/Detail.js
+++ code_br_client_side/src/components/Detail.js
@@ -1,7 +1,24 @@
-const Detail = () => {
+const Detail = ({diaryEntries}) => {
+
+const diaryEntry = diaryEntries.map((entry, index) => {
+    return (
+        <entry entry = {entry}/>
+    )
+})
+
     return(
+        <>
         <h2>Granular detail lives here</h2>
+            <ul>{diaryEntry}</ul>
+        </>
     )
 }
 
 export default Detail;
+
+// const MapFetchData = diaryEntries.map((diaryEntry) => {
+  //   return (
+  //     // <diaryEntry diaryEntry={diaryEntry} />
+  //     <p>{diaryEntry}</p>
+  //   )
+  // });
