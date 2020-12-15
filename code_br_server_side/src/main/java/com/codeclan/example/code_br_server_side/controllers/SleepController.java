99593d1c6a5949f3af1f8283f91893897b268f3a
Alan Parley <alanparley@hotmail.com>
2020-12-15 12:17:16 +0000

    sleep controller get and post ok

---
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/controllers/SleepController.java
@@ -0,0 +1,33 @@
+package com.codeclan.example.code_br_server_side.controllers;
+
+import com.codeclan.example.code_br_server_side.models.Sleep;
+import com.codeclan.example.code_br_server_side.repositories.SleepRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SleepController {
+
+    @Autowired
+    SleepRepository sleepRepository;
+
+    @GetMapping(value = "/sleep")
+    public ResponseEntity<List<Sleep>> findAllSleeps(){
+        return new ResponseEntity<>(sleepRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/sleep/{id}")
+    public ResponseEntity getSleep(@PathVariable Long id){
+        return new ResponseEntity(sleepRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/sleep")
+    public ResponseEntity<Sleep> postSleep(@RequestBody Sleep sleep){
+        Sleep newSleep = sleepRepository.save(sleep);
+        return new ResponseEntity<>(newSleep, HttpStatus.CREATED);
+    }
+}
