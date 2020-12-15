1315be910fabc7e4d3a6f91cf736fcb89c228086
Alan Parley <alanparley@hotmail.com>
2020-12-15 11:40:54 +0000

    water intake routes aok

---
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/controllers/WaterIntakeController.java
@@ -0,0 +1,33 @@
+package com.codeclan.example.code_br_server_side.controllers;
+
+import com.codeclan.example.code_br_server_side.models.WaterIntake;
+import com.codeclan.example.code_br_server_side.repositories.WaterIntakeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class WaterIntakeController {
+
+    @Autowired
+    WaterIntakeRepository waterIntakeRepository;
+
+    @GetMapping(value = "/waterintake")
+    public ResponseEntity<List<WaterIntake>> findAllWaterIntakes(){
+        return new ResponseEntity<>(waterIntakeRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/waterintake/{id}")
+    public ResponseEntity getWaterIntake(@PathVariable Long id){
+        return new ResponseEntity(waterIntakeRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/waterintake")
+    public ResponseEntity<WaterIntake> postWaterIntake(@RequestBody WaterIntake waterIntake){
+        WaterIntake newWaterIntake = waterIntakeRepository.save(waterIntake);
+        return new ResponseEntity<>(newWaterIntake, HttpStatus.CREATED);
+    }
+}
--- code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/repositories/WaterIntakeRepository.java
+++ code_br_server_side/src/main/java/com/codeclan/example/code_br_server_side/repositories/WaterIntakeRepository.java
@@ -1,10 +1,16 @@
 package com.codeclan.example.code_br_server_side.repositories;
 
+import com.codeclan.example.code_br_server_side.models.Meditation;
 import com.codeclan.example.code_br_server_side.models.WaterIntake;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WaterIntakeRepository extends JpaRepository <WaterIntake, Long> {
+
+    List<WaterIntake> findAll();
+    List<WaterIntake> findById(long id);
 
 }
