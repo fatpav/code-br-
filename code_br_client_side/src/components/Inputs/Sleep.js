f5b5c133b6fe338d807e16640390c074d160b2ea
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-15 12:06:28 +0000

    fancy slider added to sleep component as well as a submit button. submit route to be added once controllers working

--- code_br_client_side/src/components/Inputs/Sleep.js
+++ code_br_client_side/src/components/Inputs/Sleep.js
@@ -1,6 +1,37 @@
+// import classes from '*.module.css';
+import {Slider, Typography} from '@material-ui/core';
+
 const Sleep = () => {
+
+    function valuetext(value) {
+        return `${value}`;
+    }
+
+
+
     return (
-        <h2>Sleepy times go here.</h2>
+        <>
+        <div>
+            <p>How long did you sleep last night ?</p>
+                <Typography id="discrete-slider" gutterBottom>
+                    Sleepytime
+                </Typography>
+                <Slider
+                    defaultValue={5}
+                    getAriaValueText={valuetext}
+                    aria-labelledby="discrete-slider"
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={0}
+                    max={10}
+                />
+            <form 
+                action="" method="post">
+                    <button type="submit">Submit</button>
+            </form>
+      </div>
+      </>
     )
 };
 
