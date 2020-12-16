abb75756f1f0820e28c5b515085e28f59a41d136
mjmccoach <mmccoach88@gmail.com>
2020-12-16 14:49:07 +0000

    Started to bring in breathe component

--- code_br_client_side/src/components/Exercises/Breathe.js
+++ code_br_client_side/src/components/Exercises/Breathe.js
@@ -1,7 +1,17 @@
 const Breathe = () => {
     return (
         <h2>Breathe</h2>
+        <div class="watch-face">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+    </div>
+    <h2 id="text"></h2>
     )
 }
 
 export default Breathe;
+
