85de65123042a8996f27d8164572c368a542de9d
mjmccoach <mmccoach88@gmail.com>
2020-12-16 15:36:00 +0000

    Testing rendering a chart before we pull through our data and use it

--- code_br_client_side/src/components/StartScreen.js
+++ code_br_client_side/src/components/StartScreen.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import {useState} from 'react';
 import Loading from './Loading';
-import {StarOutline, ThumbUpOutline} from 'heroicons-react';
+import {ThumbUpOutline} from 'heroicons-react';
 
 const StartScreen = () =>{
 
