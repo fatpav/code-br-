36b91ba33f95134f7031feaba9c7f20e1b152152
mjmccoach <mmccoach88@gmail.com>
2020-12-16 16:18:11 +0000

    Have fully tested charts and can style to match our app

--- code_br_client_side/src/components/Tracker.js
+++ code_br_client_side/src/components/Tracker.js
@@ -3,54 +3,51 @@ import { render } from "react-dom";
 import { Chart } from "react-google-charts";
 
 const Tracker = () => {
+
+    const data = [
+        ["Date", "Litres"],
+        ["12-12-20", 1.5],
+        ["13-12-20", 2],
+        ["14-12-20", 2.5],
+        ["15-12-20", 1.4],
+        ["16-12-20", 1.4],
+        ["17-12-20", 2.4],
+        ["18-12-20", 3.1]
+      ];
+    
+
+    const options = {
+        title: "Water Intake",
+        curveType: "function",
+        legend: { position: "bottom" },
+        lineWidth: 5,
+        series: {
+            0: {color: '#191170' }
+        },
+        backgroundColor: 'transparent',
+        fontName: 'Quicksand',
+        textStyle: { color: '#F191170', 
+                   fontName: 'Quicksand', 
+                   fontSize: '10' }
+
+      };
+    
     return (
         <>
         <h2>Tracker</h2>
+      <div className="chart">
         <Chart
-  width={'500px'}
-  height={'300px'}
-  chartType="Histogram"
-  loader={<div>Loading Chart</div>}
-  data={[
-    ['Dinosaur', 'Length'],
-    ['Acrocanthosaurus (top-spined lizard)', 12.2],
-    ['Albertosaurus (Alberta lizard)', 9.1],
-    ['Allosaurus (other lizard)', 12.2],
-    ['Apatosaurus (deceptive lizard)', 22.9],
-    ['Archaeopteryx (ancient wing)', 0.9],
-    ['Argentinosaurus (Argentina lizard)', 36.6],
-    ['Baryonyx (heavy claws)', 9.1],
-    ['Brachiosaurus (arm lizard)', 30.5],
-    ['Ceratosaurus (horned lizard)', 6.1],
-    ['Coelophysis (hollow form)', 2.7],
-    ['Compsognathus (elegant jaw)', 0.9],
-    ['Deinonychus (terrible claw)', 2.7],
-    ['Diplodocus (double beam)', 27.1],
-    ['Dromicelomimus (emu mimic)', 3.4],
-    ['Gallimimus (fowl mimic)', 5.5],
-    ['Mamenchisaurus (Mamenchi lizard)', 21.0],
-    ['Megalosaurus (big lizard)', 7.9],
-    ['Microvenator (small hunter)', 1.2],
-    ['Ornithomimus (bird mimic)', 4.6],
-    ['Oviraptor (egg robber)', 1.5],
-    ['Plateosaurus (flat lizard)', 7.9],
-    ['Sauronithoides (narrow-clawed lizard)', 2.0],
-    ['Seismosaurus (tremor lizard)', 45.7],
-    ['Spinosaurus (spiny lizard)', 12.2],
-    ['Supersaurus (super lizard)', 30.5],
-    ['Tyrannosaurus (tyrant lizard)', 15.2],
-    ['Ultrasaurus (ultra lizard)', 30.5],
-    ['Velociraptor (swift robber)', 1.8],
-  ]}
-  options={{
-    title: 'Lengths of dinosaurs, in meters',
-    legend: { position: 'none' },
-  }}
-  rootProps={{ 'data-testid': '1' }}
-/>
-    
-    </>
+          chartType="LineChart"
+          width="100%"
+          height="200px"
+          data={data}
+          options={options}
+        />
+      </div>
+      </>
     )
-};
+  
+    
+    };
 
 export default Tracker;
