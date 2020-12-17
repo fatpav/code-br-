40819c5654bf27d7076d898a2843ebcefd89dd89
Alan Parley <alanparley@hotmail.com>
2020-12-17 10:50:25 +0000

    more charts added

--- code_br_client_side/src/components/Tracker.js
+++ code_br_client_side/src/components/Tracker.js
@@ -2,39 +2,101 @@ import * as React from "react";
 import { render } from "react-dom";
 import { Chart } from "react-google-charts";
 
-const Tracker = ({diaryEntry}) => {
+const Tracker = ({ diaryEntry }) => {
 
-    const data = [
-        ["Date", "Litres"],
-        [diaryEntry[0].date, diaryEntry[0].waterIntake.litres],
-        [diaryEntry[1].date, diaryEntry[1].waterIntake.litres],
-        [diaryEntry[2].date, diaryEntry[2].waterIntake.litres],
-        [diaryEntry[3].date, diaryEntry[3].waterIntake.litres],
-        [diaryEntry[4].date, diaryEntry[4].waterIntake.litres],
-        [diaryEntry[5].date, diaryEntry[5].waterIntake.litres],
-        [diaryEntry[6].date, diaryEntry[6].waterIntake.litres]
-      ];
-    
+  const data = [
+    ["Date", "Litres"],
+    [diaryEntry[0].date, diaryEntry[0].waterIntake.litres],
+    [diaryEntry[1].date, diaryEntry[1].waterIntake.litres],
+    [diaryEntry[2].date, diaryEntry[2].waterIntake.litres],
+    [diaryEntry[3].date, diaryEntry[3].waterIntake.litres],
+    [diaryEntry[4].date, diaryEntry[4].waterIntake.litres],
+    [diaryEntry[5].date, diaryEntry[5].waterIntake.litres],
+    [diaryEntry[6].date, diaryEntry[6].waterIntake.litres]
+  ];
 
-    const options = {
-        title: "Water Intake",
-        curveType: "function",
-        legend: { position: "bottom" },
-        lineWidth: 5,
-        series: {
-            0: {color: '#191170' }
-        },
-        backgroundColor: 'transparent',
-        fontName: 'Quicksand',
-        textStyle: { color: '#F191170', 
-                   fontName: 'Quicksand', 
-                   fontSize: '10' }
 
-      };
-    
-    return (
-        <>
-        <h2>Tracker</h2>
+  const options = {
+    title: "Water Intake",
+    curveType: "function",
+    legend: { position: "bottom" },
+    lineWidth: 5,
+    series: {
+      0: { color: '#191170' }
+    },
+    backgroundColor: 'transparent',
+    fontName: 'Quicksand',
+    textStyle: {
+      color: '#F191170',
+      fontName: 'Quicksand',
+      fontSize: '10'
+    }
+
+  };
+
+  const sleepData = [
+    ["Date", "Hours"],
+    [diaryEntry[0].date, diaryEntry[0].sleep.hours],
+    [diaryEntry[1].date, diaryEntry[1].sleep.hours],
+    [diaryEntry[2].date, diaryEntry[2].sleep.hours],
+    [diaryEntry[3].date, diaryEntry[3].sleep.hours],
+    [diaryEntry[4].date, diaryEntry[4].sleep.hours],
+    [diaryEntry[5].date, diaryEntry[5].sleep.hours],
+    [diaryEntry[6].date, diaryEntry[6].sleep.hours]
+  ];
+
+
+  const sleepOptions = {
+    title: "Hours slept",
+    curveType: "function",
+    legend: { position: "bottom" },
+    lineWidth: 5,
+    series: {
+      0: { color: '#191170' }
+    },
+    backgroundColor: 'transparent',
+    fontName: 'Quicksand',
+    textStyle: {
+      color: '#F191170',
+      fontName: 'Quicksand',
+      fontSize: '10'
+    }
+
+  };
+
+  const screenTimeData = [
+    ["Date", "Hours"],
+    [diaryEntry[0].date, diaryEntry[0].screenTime.hours],
+    [diaryEntry[1].date, diaryEntry[1].screenTime.hours],
+    [diaryEntry[2].date, diaryEntry[2].screenTime.hours],
+    [diaryEntry[3].date, diaryEntry[3].screenTime.hours],
+    [diaryEntry[4].date, diaryEntry[4].screenTime.hours],
+    [diaryEntry[5].date, diaryEntry[5].screenTime.hours],
+    [diaryEntry[6].date, diaryEntry[6].screenTime.hours]
+  ];
+
+
+  const screenTimeOptions = {
+    title: "Screen Time",
+    curveType: "function",
+    legend: { position: "bottom" },
+    lineWidth: 5,
+    series: {
+      0: { color: '#191170' }
+    },
+    backgroundColor: 'transparent',
+    fontName: 'Quicksand',
+    textStyle: {
+      color: '#F191170',
+      fontName: 'Quicksand',
+      fontSize: '10'
+    }
+
+  };
+
+  return (
+    <>
+      <h2>Tracker</h2>
       <div className="chart">
         <Chart
           chartType="LineChart"
@@ -44,10 +106,28 @@ const Tracker = ({diaryEntry}) => {
           options={options}
         />
       </div>
-      </>
-    )
-  
-    
-    };
+      <div className="chart">
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="200px"
+          data={sleepData}
+          options={sleepOptions}
+        />
+      </div>
+      <div className="chart">
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="200px"
+          data={screenTimeData}
+          options={screenTimeOptions}
+        />
+      </div>
+    </>
+  )
+
+
+};
 
 export default Tracker;
