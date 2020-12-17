b9275f0129cb1025fdfde49e33b3498dfdc0bee7
fatpav <fat_pav@hotmail.com>
2020-12-17 11:10:02 +0000

    all charts inputted and tracked

--- code_br_client_side/src/components/Tracker.js
+++ code_br_client_side/src/components/Tracker.js
@@ -94,6 +94,33 @@ const Tracker = ({ diaryEntry }) => {
 
   };
 
+  const meditationData = [
+    ["Date", "Minutes"],
+    [diaryEntry[0].date, diaryEntry[0].meditation.minutes],
+    [diaryEntry[1].date, diaryEntry[1].meditation.minutes],
+    [diaryEntry[2].date, diaryEntry[2].meditation.minutes],
+    [diaryEntry[3].date, diaryEntry[3].meditation.minutes],
+    [diaryEntry[4].date, diaryEntry[4].meditation.minutes],
+    [diaryEntry[5].date, diaryEntry[5].meditation.minutes],
+    [diaryEntry[6].date, diaryEntry[6].meditation.minutes]
+  ];
+  
+  const meditationOptions = {
+    title: "Meditation Time",
+    curveType: "function",
+    legend: { position: "bottom" },
+    lineWidth: 5,
+    series: {
+      0: { color: '#191170' }
+    },
+    backgroundColor: 'transparent',
+    fontName: 'Quicksand',
+    textStyle: {
+      color: '#F191170',
+      fontName: 'Quicksand',
+      fontSize: '10'
+    }
+  }
   return (
     <>
       <h2>Tracker</h2>
@@ -122,6 +149,15 @@ const Tracker = ({ diaryEntry }) => {
           height="200px"
           data={screenTimeData}
           options={screenTimeOptions}
+        />
+      </div>
+      <div className="chart">
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="200px"
+          data={meditationData}
+          options={meditationOptions}
         />
       </div>
     </>
